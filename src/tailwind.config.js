95fe6163672a9a4c4d20023ca49ef5cf2b1db098
OmarVentura <reparaciones.tonala@hotmail.com>
2026-02-09 22:34:14 -0600

    Agregando estilos y configurando tailwind css

---
+++ src/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'regalo-rosa': '#F25C92',
+        'regalo-azul-c': '#00AEEF',
+        'regalo-azul-r': '#0071BC',
+        'regalo-verde': '#7AC143',
+        'regalo-lila': '#9B51E0',
+        'regalo-amarillo': '#F2C94C',
+        'regalo-negro': '#333333',
+      },
+    },
+  },
+  plugins: [],
+}
